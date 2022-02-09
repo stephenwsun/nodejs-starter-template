318d0c23e0fd9490221edd8c3290edcf94de00b5
Stephen Sun <stephensun.me@gmail.com>
2022-02-08 21:11:01 -0600

    add test setup file and add test for user service

--- src/user/user.test.js
+++ src/user/user.test.js
@@ -1,38 +1,42 @@
-import mongoose from 'mongoose'
-import UserController from './user.controller'
+import { setupDB } from './../test-setup'
 import UserService from './user.service'
 import UserRepo from './user.repo'
 import UserModel from './user.model'
-import './../database'
+
+const DB_NAME = 'test'
+
+const mockUser = {
+  firstName: 'Stephen',
+  lastName: 'Sun',
+  email: 'test@gmail.com',
+  status: true,
+}
 
 const userRepo = new UserRepo(UserModel)
 const userService = new UserService(userRepo)
-const userController = new UserController(userService)
 
-const TEST_DB_NAME = 'test'
+setupDB(DB_NAME)
 
-describe('insert', () => {
-  beforeAll(async () => {
-    const url = `mongodb://127.0.0.1/${TEST_DB_NAME}`
-    await mongoose.connect(url, { useNewUrlParser: true })
+describe('User service', () => {
+  it('should create a user', async () => {
+    const createdUser = await userService.createUser(mockUser)
+    const insertedUser = await userRepo.findById({ _id: createdUser._id })
+
+    expect(insertedUser._id).toBeTruthy()
+    expect(insertedUser.firstName).toBeTruthy()
+    expect(insertedUser.lastName).toBeTruthy()
+    expect(insertedUser.email).toBeTruthy()
+    expect(insertedUser.status).toBeTruthy()
+
+    expect(insertedUser.firstName).toEqual(mockUser.firstName)
+    expect(insertedUser.lastName).toEqual(mockUser.lastName)
+    expect(insertedUser.email).toEqual(mockUser.email)
+    expect(insertedUser.status).toEqual(mockUser.status)
   })
+})
 
-  afterEach(async () => {
-    await UserModel.deleteMany()
-  })
-
-  afterAll(async () => {
-    await mongoose.connection.close()
-  })
-
-  it('should insert a doc into collection', async () => {
-    const mockUser = {
-      firstName: 'Stephen',
-      lastName: 'Sun',
-      email: 'test@gmail.com',
-      status: true,
-    }
-
+describe('User repo', () => {
+  it('should create a user', async () => {
     const createdUser = await userRepo.createUser(mockUser)
     const insertedUser = await userRepo.findById({ _id: createdUser._id })
 
