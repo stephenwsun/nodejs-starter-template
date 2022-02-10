d18f7b726a0e14ee74fd837e977a7e4768dc7a37
Stephen Sun <stephensun.me@gmail.com>
2022-02-09 21:26:01 -0600

    add jwt methods and error handling

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const env = process.env.NODE_ENV
 export const port = process.env.PORT
 export const logLevel = process.env.LOG_LEVEL
+export const jwtSecret = process.env.JWT_SECRET
 
 export const db = {
   name: process.env.DB_NAME || '',
